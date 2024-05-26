7d5025b064a0dc8de4cbba1a817acf35cbed089c
eliahoo danesh <danesheliahoo@gmail.com>
2024-05-26 09:58:01 +0300

    contact

--- src/comp/Contact.js
+++ src/comp/Contact.js
@@ -8,16 +8,16 @@ export default function Contact() {
         <div className="col-md-6">
           <div className="card">
             <div className="card-body">
-              <h3>אליהו דאנש - 0525721369  <br></br>גיא ברכה - 0525914445</h3>
+              <h3>אליהו דאנש - 0525721369  <br></br>danesheliahoo@gmail.com</h3>
             </div>
           </div>
         </div>
         <div className="col-md-6">
           <div className="card">
             <div className="card-body">
-              <h3>דוא"ל:</h3>
-              <p>danesheliahoo@gmail.com</p>
-              <p>guy.bracha@gmail.com</p>
+             
+            <h3>גיא ברכה - 0525914445</h3>
+            <h3>guy.bracha@gmail.com</h3>
             </div>
           </div>
         </div>
