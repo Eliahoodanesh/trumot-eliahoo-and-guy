5c17ded25b491c707cc37ca5a0cae3df4a9c5816
guybracha <guy.bracha@gmail.com>
2024-10-01 11:20:08 +0300

    add more

--- src/comp/Query.js
+++ src/comp/Query.js
@@ -6,23 +6,30 @@ import ItemDisplay from './ItemDisplay'; // Import custom ItemDisplay component
 
 export default function Query() {
   const [cities, setCities] = useState([]); // State to store the list of cities
+  const [categories, setCategories] = useState([]); // State to store the list of categories
   const [selectedCity, setSelectedCity] = useState(''); // State for the selected city
+  const [selectedCategory, setSelectedCategory] = useState(''); // State for the selected category
   const [searchTerm, setSearchTerm] = useState(''); // State for the search term
   const [results, setResults] = useState([]); // State to store search results
 
   useEffect(() => {
-    const fetchCities = async () => {
+    const fetchCitiesAndCategories = async () => {
       try {
-        const citiesSnapshot = await getDocs(collection(firestore, 'items')); // Fetch all items from Firestore
-        const citiesData = citiesSnapshot.docs.map(doc => doc.data().city); // Extract city data from items
+        const itemsSnapshot = await getDocs(collection(firestore, 'items')); // Fetch all items from Firestore
+        const citiesData = itemsSnapshot.docs.map(doc => doc.data().city); // Extract city data from items
+        const categoriesData = itemsSnapshot.docs.map(doc => doc.data().category); // Extract category data from items
+        
         const uniqueCities = [...new Set(citiesData)]; // Get unique cities
+        const uniqueCategories = [...new Set(categoriesData)]; // Get unique categories
+        
         setCities(uniqueCities); // Update state with unique cities
+        setCategories(uniqueCategories); // Update state with unique categories
       } catch (error) {
-        console.error('Error fetching cities:', error); // Log any errors
+        console.error('Error fetching cities or categories:', error); // Log any errors
       }
     };
 
-    fetchCities(); // Call the function to fetch cities on component mount
+    fetchCitiesAndCategories(); // Call the function to fetch cities and categories on component mount
   }, []);
 
   const handleSearch = async () => {
@@ -30,16 +37,39 @@ export default function Query() {
       const itemsCollection = collection(firestore, 'items'); // Reference to items collection
       let q; // Query variable
 
-      // Build the Firestore query based on selected city and search term
-      if (selectedCity && searchTerm) {
+      // Build the Firestore query based on selected city, category, and search term
+      if (selectedCity && selectedCategory && searchTerm) {
+        q = query(
+          itemsCollection,
+          where('city', '==', selectedCity), // Filter by selected city
+          where('category', '==', selectedCategory), // Filter by selected category
+          where('itemName', '>=', searchTerm), // Filter by search term
+          where('itemName', '<=', searchTerm + '\uf8ff') // Include all items matching the search term
+        );
+      } else if (selectedCity && selectedCategory) {
+        q = query(
+          itemsCollection,
+          where('city', '==', selectedCity), // Filter by selected city
+          where('category', '==', selectedCategory) // Filter by selected category
+        );
+      } else if (selectedCity && searchTerm) {
         q = query(
           itemsCollection,
           where('city', '==', selectedCity), // Filter by selected city
           where('itemName', '>=', searchTerm), // Filter by search term
-          where('itemName', '<=', searchTerm + '\uf8ff') // Include all items matching the search term
+          where('itemName', '<=', searchTerm + '\uf8ff')
+        );
+      } else if (selectedCategory && searchTerm) {
+        q = query(
+          itemsCollection,
+          where('category', '==', selectedCategory), // Filter by selected category
+          where('itemName', '>=', searchTerm), // Filter by search term
+          where('itemName', '<=', searchTerm + '\uf8ff')
         );
       } else if (selectedCity) {
         q = query(itemsCollection, where('city', '==', selectedCity)); // Filter by selected city only
+      } else if (selectedCategory) {
+        q = query(itemsCollection, where('category', '==', selectedCategory)); // Filter by selected category only
       } else if (searchTerm) {
         q = query(
           itemsCollection,
@@ -69,7 +99,7 @@ export default function Query() {
         <Row className='justify-content-center'>
           <Col xs={12} md={8} lg={6}>
             <Card className='p-4 shadow'>
-              <h1 className='text-center mb-4'>חפש לפי עיר ופריט</h1> {/* Search header */}
+              <h1 className='text-center mb-4'>חפש לפי עיר וקטגוריה</h1> {/* Search header */}
               <Row className='text-center mb-3'>
                 <Col sm={12} className='mb-3'>
                   <Form.Group>
@@ -82,6 +112,22 @@ export default function Query() {
                       {cities.map((city, index) => ( // Map through cities to create options
                         <option key={index} value={city}>
                           {city}
+                        </option>
+                      ))}
+                    </Form.Control>
+                  </Form.Group>
+                </Col>
+                <Col sm={12} className='mb-3'>
+                  <Form.Group>
+                    <Form.Control
+                      as="select" // Dropdown for selecting a category
+                      value={selectedCategory}
+                      onChange={(e) => setSelectedCategory(e.target.value)} // Update state on category selection
+                    >
+                      <option value="">בחר קטגוריה</option> {/* Default option */}
+                      {categories.map((category, index) => ( // Map through categories to create options
+                        <option key={index} value={category}>
+                          {category}
                         </option>
                       ))}
                     </Form.Control>
--- src/comp/Upload.js
+++ src/comp/Upload.js
@@ -7,7 +7,6 @@ import { collection, doc, serverTimestamp, writeBatch } from 'firebase/firestore
 import { getAuth } from 'firebase/auth';
 
 export default function Upload() {
-  // State variables to manage image previews, selected files, and form inputs
   const [imagePreviews, setImagePreviews] = useState([]);
   const [selectedFiles, setSelectedFiles] = useState([]);
   const [cities, setCities] = useState([]);
@@ -20,122 +19,131 @@ export default function Upload() {
   const [donorName, setDonorName] = useState('');
   const [donorPhoneNumber, setDonorPhoneNumber] = useState('');
   const [donorEmail, setDonorEmail] = useState('');
+  const [categories] = useState([
+    'ביגוד',
+    'הנעלה',
+    'משחקים',
+    'ציוד ספורט',
+    'אלקטרוניקה',
+    'ספרים',
+    'רהיטים',
+    'כלי מטבח',
+    'אביזרי נוי',
+    'ציוד גן',
+  ]);
+  const [selectedCategory, setSelectedCategory] = useState(''); // Added category state
+
 
   const auth = getAuth(); // Initialize Firebase Auth
 
-  // API URL for fetching city data
   const apiUrl =
     'https://data.gov.il/api/3/action/datastore_search?resource_id=d4901968-dad3-4845-a9b0-a57d027f11ab&limit=1272';
 
-  // Fetch cities on component mount
   useEffect(() => {
     async function fetchCities() {
       try {
         const response = await fetch(apiUrl);
         const data = await response.json();
-        // Map the city records to an array of objects with id and name
         const cityRecords = data.result.records.map(record => ({
           id: record._id,
           name: record['שם_ישוב'],
         }));
-        setCities(cityRecords); // Update state with city records
+        setCities(cityRecords);
       } catch (error) {
         console.error('Error fetching cities:', error);
       }
     }
 
-    fetchCities(); // Call the fetchCities function
+    fetchCities();
   }, []);
 
-  // Handle file input change
   const handleFileChange = event => {
     const files = event.target.files;
-    setSelectedFiles(files); // Store selected files in state
+    setSelectedFiles(files);
     const newImagePreviews = [];
 
-    // Generate image previews for selected files
     Array.from(files).forEach(file => {
       const reader = new FileReader();
       reader.onloadend = () => {
         newImagePreviews.push(reader.result);
         if (newImagePreviews.length === files.length) {
-          setImagePreviews([...imagePreviews, ...newImagePreviews]); // Update previews state
+          setImagePreviews([...imagePreviews, ...newImagePreviews]);
         }
       };
-      reader.readAsDataURL(file); // Read file as data URL for preview
+      reader.readAsDataURL(file);
     });
   };
 
-  // Handle upload of selected files and item data
   const handleUpload = async () => {
     try {
-      // Validate required fields before proceeding
-      if (!itemName || !itemDescription || !donorName || (!donorPhoneNumber && !donorEmail)) {
+      // Validate required fields
+      if (!itemName || !itemDescription || !donorName || (!donorPhoneNumber && !donorEmail) || !selectedCategory) {
         alert('Please fill in all required fields: Item Name, Description, Donor Name, and either Phone or Email.');
         return;
       }
-
+  
       const user = auth.currentUser; // Get the current authenticated user
       if (!user) {
         alert('You need to be logged in to upload items.');
         return;
       }
-
-      const batch = writeBatch(firestore); // Start a Firestore batch write
+  
+      const batch = writeBatch(firestore);
       const uploadedUrls = [];
-
-      // Loop through selected files and upload each to Firebase Storage
+  
       for (const file of selectedFiles) {
         try {
           console.log('Uploading file:', file.name);
-          const imgRef = ref(imageDb, `uploadsImage/${uuidv4()}`); // Create a reference with a unique ID
-          const snapshot = await uploadBytes(imgRef, file); // Upload the file
-          const url = await getDownloadURL(snapshot.ref); // Get the download URL
-          uploadedUrls.push(url); // Store the URL
+          const imgRef = ref(imageDb, `uploadsImage/${uuidv4()}`);
+          const snapshot = await uploadBytes(imgRef, file);
+          const url = await getDownloadURL(snapshot.ref);
+          uploadedUrls.push(url);
           console.log('File uploaded:', url);
         } catch (uploadError) {
           console.error('Error uploading file:', file.name, uploadError);
-          throw new Error('File upload failed'); // Handle upload errors
+          throw new Error('File upload failed');
         }
       }
-
-      // Set phone number status based on user approval
+  
       const phoneStatus = phoneApproved ? 'Your phone number' : 'מספר לא לפרסום';
-
-      // Create item data to be stored in Firestore
+  
       const itemData = {
         userId: user.uid, // Store the user's UID
         itemName: itemName,
         itemDescription: itemDescription,
         itemNote: itemNote,
         city: selectedCity,
+        category: selectedCategory,
         imageUrls: uploadedUrls,
         phoneStatus: phoneStatus,
         donorName: donorName,
         donorPhoneNumber: phoneApproved ? donorPhoneNumber : 'מספר לא לפרסום',
         donorEmail: donorEmail,
-        createdAt: serverTimestamp(), // Timestamp for item creation
+        createdAt: serverTimestamp(),
       };
-
+  
       const itemsCollection = collection(firestore, 'items');
-      const newItemRef = doc(itemsCollection); // Create a new document reference
-      batch.set(newItemRef, itemData); // Add item data to batch
-
-      await batch.commit(); // Commit the batch write to Firestore
-      setUploadedUrls(uploadedUrls); // Update state with uploaded URLs
-      alert('All files uploaded successfully!'); // Success message
+      const newItemRef = doc(itemsCollection);
+      batch.set(newItemRef, itemData);
+  
+      await batch.commit();
+      setUploadedUrls(uploadedUrls);
+      alert('All files uploaded successfully!');
     } catch (error) {
       console.error('Error during Firestore write operation:', error);
-      alert('Failed to write item data. Please try again.'); // Error handling
+      alert('Failed to write item data. Please try again.');
     }
   };
+  
 
-  // Handle city selection change
   const handleCityChange = event => {
     setSelectedCity(event.target.value);
   };
 
-  // Handle phone number visibility approval
+  const handleCategoryChange = event => { // Handle category change
+    setSelectedCategory(event.target.value);
+  };
+
   const handlePhoneApproval = event => {
     setPhoneApproved(event.target.checked);
   };
@@ -155,7 +163,7 @@ export default function Upload() {
                 accept='image/*'
                 multiple
                 onChange={handleFileChange}
-                style={{ display: 'none' }} // Hide the file input
+                style={{ display: 'none' }}
               />
               <label htmlFor='file-upload' className='btn btn-primary'>
                 העלה תמונות
@@ -179,7 +187,7 @@ export default function Upload() {
                   className='form-control'
                   placeholder='שם פריט'
                   value={itemName}
-                  onChange={e => setItemName(e.target.value)} // Update item name state
+                  onChange={e => setItemName(e.target.value)}
                 />
               </div>
               <div className='col-md-4'>
@@ -188,7 +196,7 @@ export default function Upload() {
                   className='form-control'
                   placeholder='תיאור'
                   value={itemDescription}
-                  onChange={e => setItemDescription(e.target.value)} // Update item description state
+                  onChange={e => setItemDescription(e.target.value)}
                 />
               </div>
               <div className='col-md-4'>
@@ -197,14 +205,14 @@ export default function Upload() {
                   className='form-control'
                   placeholder='הערות'
                   value={itemNote}
-                  onChange={e => setItemNote(e.target.value)} // Update item note state
+                  onChange={e => setItemNote(e.target.value)}
                 />
               </div>
               <div className='col-md-4'>
                 <select
                   className='form-control'
                   value={selectedCity}
-                  onChange={handleCityChange} // Update selected city state
+                  onChange={handleCityChange}
                 >
                   <option value='' disabled defaultValue>
                     בחר עיר
@@ -217,6 +225,22 @@ export default function Upload() {
                 </select>
               </div>
             </div>
+            <div className='col-md-4'>
+                <select
+                  className='form-control'
+                  value={selectedCategory}
+                  onChange={handleCategoryChange}
+                >
+                  <option value='' disabled defaultValue>
+                    בחר קטגוריה
+                  </option>
+                  {categories.map((category, index) => (
+                    <option key={index} value={category}>
+                      {category}
+                    </option>
+                  ))}
+                </select>
+            </div>
             <div className='form-group'>
               <input
                 type='text'
@@ -224,7 +248,7 @@ export default function Upload() {
                 id='donorName'
                 placeholder='שם התורם'
                 value={donorName}
-                onChange={e => setDonorName(e.target.value)} // Update donor name state
+                onChange={e => setDonorName(e.target.value)}
               />
             </div>
             <div className='form-check'>
@@ -239,38 +263,41 @@ export default function Upload() {
                 className='form-check-input'
                 id='phone-approval'
                 checked={phoneApproved}
-                onChange={handlePhoneApproval} // Handle phone approval checkbox change
+                onChange={handlePhoneApproval}
               />
             </div>
             {phoneApproved && (
+             <div className='form-group'>
+              אנא הכנס מספרים בלבד, ללא תווים נוספים.
+             <input
+               type='tel'
+               className='form-control'
+               id='donorPhoneNumber'
+               placeholder='מספר הטלפון'
+               value={donorPhoneNumber}
+               maxLength='10' // הגבלת אורך הקלט
+               onChange={e => {
+                 // מניעת קלט של תווים לא רצויים
+                 const inputVal = e.target.value.replace(/[^\d-]/g, '').slice(0, 10);
+                 setDonorPhoneNumber(inputVal);
+               }}
+             />
+           </div>
+            )}
+            {!phoneApproved && (
               <div className='form-group'>
-                אנא הכנס מספרים בלבד
                 <input
-                  type='tel'
+                  type='email'
                   className='form-control'
-                  id='donorPhoneNumber'
-                  placeholder='טלפון'
-                  value={donorPhoneNumber}
-                  onChange={e => setDonorPhoneNumber(e.target.value)} // Update donor phone number state
+                  id='donorEmail'
+                  placeholder='כתובת הדוא"ל שלך'
+                  value={donorEmail}
+                  onChange={e => setDonorEmail(e.target.value)}
                 />
               </div>
             )}
-            <div className='form-group'>
-              <input
-                type='email'
-                className='form-control'
-                id='donorEmail'
-                placeholder='אימייל (לא חובה)'
-                value={donorEmail}
-                onChange={e => setDonorEmail(e.target.value)} // Update donor email state
-              />
-            </div>
-            <div className='text-center'>
-              <button
-                type='button'
-                className='btn btn-success'
-                onClick={handleUpload} // Trigger upload on button click
-              >
+            <div className='d-flex justify-content-center'>
+              <button className='btn btn-primary' onClick={handleUpload}>
                 העלה פריט
               </button>
             </div>
